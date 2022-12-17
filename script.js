3ab596f7c9b6cd10a0952adf4704119d73864657
Justin Elliott <thecrashtestjustin@gmail.com>
2022-12-17 14:30:59 -0800

    added password matching form validation

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const createAccount = document.querySelector('.create-account');
-const pw = document.querySelector('#pw');
-const pw2 = document.querySelector('#pw2');
-const email = document.getElementById('email');
-const firstname = document.getElementById('firstname');
-const lastname = document.getElementById('lastname');
-const tel = document.getElementById('tel');
-const form = document.querySelector('form');
+const form = document.getElementById('form');
+const pw = document.getElementById('pw');
+const pw2 = document.getElementById('pw2');
+const pwMissmatch = document.getElementById('pw-valid');
 const input = document.querySelectorAll('input');
-const requiredMessage = document.querySelectorAll('span.required-field-notice');
-const secondpw = document.getElementById('pw-valid');
+const requiredFieldNotice = document.querySelectorAll('span.required-field-notice');
 
+form.addEventListener('submit', e => {
+    let messages = [];
+    if (pw.value !== pw2.value) {
+        pwMissmatch.className = 'password-missmatch'
+        pw.className = 'invalid-password-inputs';
+        pw2.className = 'invalid-password-inputs';
+        e.preventDefault();
+    }
+});
+
+
+pw2.addEventListener('input', e => {
+    if (pw.value !== pw2.value) {
+        pwMissmatch.className = 'password-missmatch';
+        pw.className = 'invalid-password-inputs';
+        pw2.className = 'invalid-password-inputs';
+    } else {
+        pwMissmatch.className = 'required-field-notice';
+        pw.classList.remove('invalid-password-inputs');
+        pw2.classList.remove('invalid-password-inputs');
+    }
+});
 
 
 
