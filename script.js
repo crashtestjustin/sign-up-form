abb318b43783dcf6f2e10ce9775ada8d23e6df0a
Justin Elliott <thecrashtestjustin@gmail.com>
2022-12-19 07:01:22 -0800

    adds password validation for a regexp

--- script.js
+++ script.js
@@ -7,21 +7,24 @@ const input = document.querySelectorAll('input');
 const requiredFieldNotice = document.querySelectorAll('.required-field-notice');
 const inputValideCheck = document.getElementById('input-valid-check');
 
+pwPattern = new RegExp('^(?=.*[A-Z])[a-zA-Z0-9]{4,}$');
+
 //need to set up form validation for all required fields on blanks and invalid data
 
 form.addEventListener('submit', e => {
-    let messages = [];
-    pwPattern = "^(?=.*[A-Z])[a-zA-Z0-9]{4,}$";
+    password = pw.value;
     if (pw.value !== pw2.value) {
         pwMissmatch.className = 'password-missmatch'
         pw.className = 'invalid-password-inputs';
         pw2.className = 'invalid-password-inputs';
         e.preventDefault();
     } //NEED TO FIGURE OUT HOW TO TEST AGAINST A REGEXP FOR THE PASSWORD STYLE
-    if (pw.value.length < 4 || RegExp.pwPattern.test(pw.value) == false) {
+    if (pw.value.length < 4 || !pwPattern.test(password)) {
         pwCheck.className = ".required-field-notice password-invalid";
         pw.className = 'invalid-password-inputs';
         e.preventDefault();
+    } else {
+        pwCheck.classList.remove('password-invalid');
     }
 });
 
@@ -36,6 +39,14 @@ pw2.addEventListener('input', e => {
         pw2.classList.remove('invalid-password-inputs');
     }
 });
+
+pw.addEventListener('input', e => {
+    const password = pw.value;
+    if (pw.value.length >= 4 && pwPattern.test(password)) {
+        pwCheck.classList.remove('password-invalid');
+        pw.classList.remove('invalid-password-inputs');
+    }
+})
 
 
 
